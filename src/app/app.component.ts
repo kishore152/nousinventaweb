75beef4e6f3c544c841b0a82bd46424bb8981208
kishore.kumar <kishore.kumar@kgisl.com>
2025-03-29 14:43:35 +0530

    aipage chnages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'beta';
 
+  constructor(private route: ActivatedRoute, private router: Router){
+  
+    }
+  
+     
+    
+    navigateTo(ind:any){
+      console.log(ind)
+      this.router.navigate(['/'+ind])
+    }
 }
